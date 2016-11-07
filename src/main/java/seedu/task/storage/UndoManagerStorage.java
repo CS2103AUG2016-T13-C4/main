506e591075aa376271640505b2c34e3ff048145b
oqyxxy <oqyxxy93@gmail.com>
2016-11-07 10:59:39 +0800

    cleaned up unused code for undo and redo

--- src/main/java/seedu/task/storage/UndoManagerStorage.java
+++ src/main/java/seedu/task/storage/UndoManagerStorage.java
@@ -9,49 +9,17 @@ import seedu.task.model.task.Task;
 public class UndoManagerStorage {
     private Stack<UserAction> undoStack;
     private Stack<UserAction> redoStack;
-    /*
-    private Vector<UndoManagerStorage> storedTasksUndone;
-    private Vector<UndoManagerStorage> storedTasksDone;
-    
-    private Task task;
-    private String commandWord;
-    private int index;
-    */
-    
+
     public UndoManagerStorage(Vector<UndoManagerStorage> storedTasksUndone, 
            Vector<UndoManagerStorage> storedTasksDone ) {
         this.undoStack = new Stack<UserAction>();
         this.redoStack = new Stack<UserAction>();
-        /*
-        this.storedTasksUndone = storedTasksUndone;
-        this.storedTasksDone = storedTasksDone;
-        */
     }
     
     public void recordAction(UserAction toRecord) {
     	undoStack.push(toRecord);
     }
-    
-//    public void undoUpdate(UndoManagerStorage undoM) {
-//        storedTasksUndone.add(undoM);
-//    }
-//
-//  
-//    /**
-//     * Getters
-//     */
-//    public String getCommandWord() {
-//        return commandWord;
-//    }
-//    
-//    public int getIndex() {
-//        return index;
-//    }
-//    
-//    public Task getTask() {
-//        return task;
-//    }
-//    
+
     public Stack<UserAction> getUndoStack() {
         return undoStack;
     }
@@ -59,18 +27,7 @@ public class UndoManagerStorage {
     public Stack<UserAction> getRedoStack() {
         return redoStack;
     }
-//    
-//    public Vector<UndoManagerStorage> getStoredTasksUndone() {
-//        return storedTasksUndone;
-//    }
-//    
-//    public Vector<UndoManagerStorage> getStoredTasksDone() {
-//        return storedTasksDone;
-//    }
-//
-//    
-                
-    
+
 }
 
 
